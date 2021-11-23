14c6d47948e45242bb228ab1f0303fb3744067b6
carinaghiorghita <carinaioana090100@gmail.com>
2021-11-23 22:53:55 +0200

    Add search method to trie

--- Trie/src/ubb/research/Model/Trie.java
+++ Trie/src/ubb/research/Model/Trie.java
@@ -41,4 +41,37 @@ public class Trie {
         currentNode.setEndOfWord(true);
     }
 
+    public boolean search(String key) {
+        if(key.length() == 0)
+            return true;
+
+        var length = key.length();
+        var currentNode = root;
+        var currentIndex = 0;
+
+        while (currentIndex < length) {
+            var currentChar = key.charAt(currentIndex);
+
+            var children = currentNode.getChildren();
+            var isFound = false;
+            for(Node node : children)
+                if(node.getValue() == currentChar){
+                    isFound = true;
+                    currentNode = node;
+                    break;
+                }
+            if(!isFound)
+                return false;
+
+            currentIndex++;
+        }
+
+        return currentNode.isEndOfWord();
+    }
+
+
+
+    public Node getRoot() {
+        return root;
+    }
 }
